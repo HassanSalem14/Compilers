60fbca5dfec0196122ced57fe1e117b1233d66a5
HassanSalem14 <87920108+HassanSalem14@users.noreply.github.com>
2023-02-14 12:20:44 -0500

    Rename MainReader.c to Reader/MainReader.c

--- Reader/MainReader.c
+++ Reader/MainReader.c
@@ -38,7 +38,7 @@
 ************************************************************
 * File name: MainReader.c
 * Compiler: MS Visual Studio 2022
-* Course: CST 8152 – Compilers, Lab Section: [011, 012, 013]
+* Course: CST 8152 â€“ Compilers, Lab Section: [011, 012, 013]
 * Assignment: A12, A22, A32.
 * Date: Sep 01 2022
 * Professor: Paulo Sousa
